4c8316317e093e20eafe43c6ba7bb76beebfa2aa
peterlpk10-lang <peterlpk10@gmail.com>
2026-07-01 17:02:49 -0300

    questão 3 dicionário

--- Lab9/dicionario.py
+++ Lab9/dicionario.py
@@ -9,49 +9,48 @@ resultado = contagem_caracteres(frase)
 print(resultado)
 
 # questao 02
+#arquivo = open('C:/Users/Admin/OneDrive/Documentos/GitHub/atividades_individuais_poo/Lab9/estomago.txt', 'r', encoding='utf-8')   
+#texto = arquivo.read()
+#alfabeto = {
+#    'a': 1,
+#    'b': 2,
+#    'c': 3,
+#    'd': 4, 
+#    'e': 5,
+#    'f': 6,
+#    'g': 7,
+#    'h': 8,
+#    'i': 9,
+#    'j': 10,
+#    'k': 11,
+#    'l': 12,
+#    'm': 13,
+#    'n': 14,
+#    'o': 15,
+#    'p': 16,
+#    'q': 17,
+#    'r': 18,
+#    's': 19,
+#    't': 20,
+#    'u': 21,
+#    'v': 22,
+#    'w': 23,
+#    'x': 24,
+#    'y': 25,
+#    'z': 26
+#}
+#quant = {}
+#for i in alfabeto:
+#    quant[i] = texto.count(i)
 
-arquivo = open('C:/Users/Admin/OneDrive/Documentos/GitHub/atividades_individuais_poo/Lab9/estomago.txt', 'r', encoding='utf-8')   
-texto = arquivo.read()
-alfabeto = {
-    'a': 1,
-    'b': 2,
-    'c': 3,
-    'd': 4, 
-    'e': 5,
-    'f': 6,
-    'g': 7,
-    'h': 8,
-    'i': 9,
-    'j': 10,
-    'k': 11,
-    'l': 12,
-    'm': 13,
-    'n': 14,
-    'o': 15,
-    'p': 16,
-    'q': 17,
-    'r': 18,
-    's': 19,
-    't': 20,
-    'u': 21,
-    'v': 22,
-    'w': 23,
-    'x': 24,
-    'y': 25,
-    'z': 26
-}
-quant = {}
-for i in alfabeto:
-    quant[i] = texto.count(i)
-
-sorted(quant.values(), reverse=True)
-print(quant)
+#sorted(quant.values(), reverse=True)
+#print(quant)
 
 # questao 03
 
 def mesclar_dicionarios(d1, d2):
-    if len(d1) > len(d2):
-        for i in d1:
+    for i in range(len(d1.keys())):
+        
             
     
 dicionario1 = {'a': 1, 'b': 2, 'c': 3}
