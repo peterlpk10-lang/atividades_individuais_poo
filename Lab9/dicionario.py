c3ff350ab49d06598c3f498b1e50cd0e6be577e4
peterlpk10-lang <peterlpk10@gmail.com>
2026-07-03 09:31:54 -0300

    djicionario

--- Lab9/dicionario.py
+++ Lab9/dicionario.py
@@ -49,15 +49,50 @@ print(resultado)
 # questao 03
 
 def mesclar_dicionarios(d1, d2):
-    lista = []
+    novo_dic = {}
     for chave, valor in d1.items():
-        if d1[chave] == d2[chave]:
-            lista.append()
-            if d1[valor] > d2[valor]:
-                lista.append() 
-            
+        novo_dic[chave] = valor
+    for chave, valor in d2.items():
+        if chave in novo_dic:
+            if valor > novo_dic[chave]:
+                novo_dic[chave] = valor
+        else:
+            novo_dic[chave] = valor
+    return novo_dic
     
 dicionario1 = {'a': 1, 'b': 2, 'c': 3}
 dicionario2 = {'b': 4, 'd': 5}
 resultado = mesclar_dicionarios(dicionario1, dicionario2)
 print(resultado)
+
+# questao 04
+
+def filtrar_dicionario(dic, lista):
+    novo_dic = {}
+    for chave, valor in dic.items():
+        if chave in lista:
+            novo_dic[chave] = valor
+    return novo_dic
+
+dados = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5}
+chaves_filtradas = ['a', 'c', 'e']
+resultado = filtrar_dicionario(dados, chaves_filtradas)
+print(resultado)
+
+# questao 05
+
+def resultado_votacao(votos):
+    novo_dic = []
+    for i in votos:
+        for chave, valor in i.items():
+            novo_dic.append(chave) 
+    return dict(novo_dic)
+
+votos = [
+    {'candidato_A': 120, 'candidato_B': 85, 'candidato_C': 90},
+    {'candidato_A': 110, 'candidato_B': 95, 'candidato_C': 80},
+    {'candidato_A': 130, 'candidato_B': 78, 'candidato_C': 105},
+]
+resultado = resultado_votacao(votos)
+print(resultado)  # Saída esperada: {'candidato_A': (360, 40.31), 'candidato_B': (258, 28.89), 
+                                ###  'candidato_C': (275, 30.79)}
